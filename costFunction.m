4b90f73da755ebe845f6d97337cdfbf77f81f35d
Eugenio Kuri <eugenio.kuri@honeywell.com>
2017-12-28 17:00:34 -0600

    initial commit

---
+++ costFunction.m
@@ -0,0 +1,47 @@
+function [J, grad] = costFunction(theta, X, y)
+%COSTFUNCTION Compute cost and gradient for logistic regression
+%   J = COSTFUNCTION(theta, X, y) computes the cost of using theta as the
+%   parameter for logistic regression and the gradient of the cost
+%   w.r.t. to the parameters.
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta))
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta.
+%               You should set J to the cost.
+%               Compute the partial derivatives and set grad to the partial
+%               derivatives of the cost w.r.t. each parameter in theta
+%
+% Note: grad should have the same dimensions as theta
+%
+
+
+
+
+aux = 0;
+for j=1:m
+	aux += (-y(j) * log(sigmoid(theta'*X(j,:)'))) - ( (1-y(j)) * log(1-sigmoid(theta'*X(j,:)'))  );
+end
+J = aux/m;
+%J = sum(sum( (-y * log(sigmoid(theta'*X'))) - ( (1-y) * log(1-sigmoid(theta'*X')) ) )/m)/m;
+for i=1:size(theta)(1)
+	aux = 0;
+	for j=1;m
+		aux += (sigmoid(theta'*X(j,:)') - y(j))*X(j,i);
+	end
+	grad(i) = aux/m;
+
+%grad = sum((sigmoid(theta'*X') - y) * X)'/m;
+
+%for j = 1:size(theta)
+%	grad(j) = sum(sigmoid(sum(theta'*X(j,:)')))
+%end
+
+% =============================================================
+end
+grad
